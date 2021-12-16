6cd746b854cad7dc7cdbbd4249bff820c8e78348
guy-watson <95031920+guy-watson@users.noreply.github.com>
2021-12-16 17:10:39 +0000

    Update covid_data_handler.py
    Added docstrings and general formatting

--- covid_data_handler.py
+++ covid_data_handler.py
@@ -1,11 +1,21 @@
+'''Main data handler, takes covid data using uk_covid19 API,
+then that data is parsed and processed to generate figures used
+in the main interface'''
 import csv
+import json
 from uk_covid19 import Cov19API
-import scheduler
+
+with open('config', 'r', encoding='UTF-8')as f:
+    config_file = json.load(f)
+    filepaths = config_file['filepaths']
+    national_path = filepaths["covid-data-national"]
+    print(national_path)
 
 
 def parse_csv_data(csv_filename):
+    '''parses csv data '''
     covid_csv_data = []
-    with open(csv_filename, 'r') as covid_data_file:
+    with open(csv_filename, 'r', encoding='UTF-8') as covid_data_file:
         csvreader = csv.reader(covid_data_file)
         for row in csvreader:
             covid_csv_data.append(row)
@@ -13,12 +23,14 @@ def parse_csv_data(csv_filename):
 
 
 def process_covid_csv_data(covid_csv_data):
+    '''gets cases in last 7 days, hospital cases,
+    and total deaths from parsed csv file'''
     for data_line in covid_csv_data[1:2]:
         try:
             hospital_cases = data_line[5:]
             string = [str(i) for i in hospital_cases]
-            string_joined = ''.join(string)
-            current_hospital_cases = int(string_joined)
+            current_hospital_cases = ''.join(string)
+            current_hospital_cases = int(current_hospital_cases)
         except ValueError:
             continue
 
@@ -51,6 +63,7 @@ def process_covid_csv_data(covid_csv_data):
 
 
 def covid_API_request(location='exeter', location_type='ltla'):
+    '''takes data from the covid api'''
     exeter_filter = [
         'areaType=' + location_type,
         'areaName=' + location
@@ -70,10 +83,12 @@ def covid_API_request(location='exeter', location_type='ltla'):
         structure=cases_and_deaths,
     )
 
-    data = api.get_csv(save_as='venv\covid_data\covid_data.csv')
+    data = api.get_csv(save_as='covid-data-national.csv')
+    return data
 
 
 def covid_api_request_national(location='england', location_type='nation'):
+    '''api request that returns national data only'''
     national_filter = [
         'areaType=' + location_type,
         'areaName=' + location
@@ -93,7 +108,9 @@ def covid_api_request_national(location='england', location_type='nation'):
         structure=cases_and_deaths,
     )
 
-    data = api.get_csv(save_as='venv\covid_data\covid_data_national.csv')
+    data = api.get_csv(save_as=national_path)
+    return data
 
 
 covid_api_request_national()
+print(process_covid_csv_data(parse_csv_data('covid_data_national.csv')))
