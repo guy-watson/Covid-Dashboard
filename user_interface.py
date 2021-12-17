ac00471d71179a8ed71f36ca48ac4d671f927d88
guy-watson <95031920+guy-watson@users.noreply.github.com>
2021-12-17 05:24:00 +0000

    Update user_interface.py
    type hinting

--- user_interface.py
+++ user_interface.py
@@ -4,30 +4,33 @@ the html template. The module also handles the deletion of toasts'''
 import logging
 from flask import Flask, render_template, redirect, request
 from covid_data_handler import process_covid_csv_data, parse_csv_data
-from covid_news_handling import update_news, news_API_request
+from covid_news_handling import update_news, news_API_request, update_sched
 
 logging.basicConfig(filename='user_interface.log',
                     level=logging.INFO, format='%(levelname)s:%(name)s:%(message)s')
 
 # creates the toast
 toast = update_news(news_API_request())
+sched_toast = update_sched()
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def goto_index():
+def goto_index() -> None:
     '''This function redirects the default url to /index'''
     return redirect('/index', code=302)
 
 
 @app.route('/index')
-def main_page():
+def main_page() -> None:
     '''handles the html template and displays data on the main page'''
     delete_toast()
+    update()
     return render_template('index.html',
                            image='download.jpeg',
                            title='Covid Daily Update',
+                           updates=sched_toast,
                            news_articles=toast,
                            location='Exeter',
                            local_7day_infections=
@@ -45,7 +48,26 @@ def main_page():
                                    r'venv\covid_data\covid_data_national.csv'))[2]))
 
 
-def delete_toast():
+def update() -> None:
+    sched_toast = []
+    update_time = request.args.get('update')
+    update_title = request.args.get('two')
+    content = {'title': update_title,
+                    'content': update_time}
+    sched_toast.append(content)
+    return sched_toast
+
+
+def delete_sched_toast() -> None:
+    remove_toast = request.args.get('notif')
+    for dictionary in sched_toast:
+        if remove_toast == dictionary['title']:
+            sched_toast.remove(dictionary)
+            return sched_toast
+        return None
+
+
+def delete_toast() -> None:
     '''allows the user to remove news articles from the main page'''
     remove_toast = request.args.get('notif')
     for dictionary in toast:
@@ -58,3 +80,4 @@ def delete_toast():
 
 if __name__ == '__main__':
     app.run()
+
