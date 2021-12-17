d71ac27c86076364cc48fa803c67231276f7c66e
guy-watson <95031920+guy-watson@users.noreply.github.com>
2021-12-17 05:24:40 +0000

    Update covid_news_handling.py
    type hinting

--- covid_news_handling.py
+++ covid_news_handling.py
@@ -6,7 +6,7 @@ import sched
 import time
 import requests
 
-with open('config', 'r', encoding='UTF-8S')as f:
+with open('config', 'r', encoding='UTF-8')as f:
     config_file = json.load(f)
     api_keys = config_file['api-keys']
 
@@ -17,7 +17,7 @@ logging.basicConfig(filename='covid_news_handling.log', level=logging.INFO,
                     format='%(levelname)s:%(name)s:%(message)s')
 
 
-def news_API_request(covid_terms=['Covid', 'COVID-19', 'coronavirus']):
+def news_API_request(covid_terms=['Covid', 'COVID-19', 'coronavirus']) -> dict:
     '''Gets news data from news api'''
     url = 'https://newsapi.org/v2/top-headlines?'
     for val in covid_terms:
@@ -31,7 +31,7 @@ def news_API_request(covid_terms=['Covid', 'COVID-19', 'coronavirus']):
         return response_json
 
 
-def update_news(data):
+def update_news(data) -> dict:
     '''creates toasts from news data'''
     title_list = []
     content_list = []
@@ -54,3 +54,9 @@ def update_news(data):
         new_toast.append(content)
     logging.info('New Toast created')
     return new_toast
+
+
+def update_sched():
+    toast = {'title': 'Select title',
+            'content': 'Select time'}
+    return toast
